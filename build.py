160516bd4c69bdbb8acda93bc5bb5b4710fe50bb
Igor Montagner <igorsm1@insper.edu.br>
2025-10-13 10:57:45 -0300

    atualizando aqui

--- build.py
+++ build.py
@@ -56,7 +56,7 @@ for f in all_files:
         sh(f'{PANDOC_PAGE} {PANDOC_VARS} --resource-path {dir} {f} -o {resname}')
     else:
         if show_file_list:
-            print(resname)
+            print(f)
         else:
             print('copy', f)
         sh(f'cp {f} {without_src}')
