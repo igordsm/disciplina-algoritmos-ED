7c26e5affcf42733247e7f64f638200900e923ef
Igor Montagner <igorsm1@insper.edu.br>
2026-02-19 17:23:56 -0300

    atualização 2026 parte 2

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ DATE_CMD = 'date +\"%d/%m/%Y %H:%M\"'
 PANDOC_HANDOUT = 'pandoc -f markdown+pipe_tables+backtick_code_blocks+fenced_divs+raw_html --lua-filter=filters/message.lua --lua-filter=filters/spacer.lua --lua-filter=filters/graphviz.lua --lua-filter=filters/side-by-side.lua  -s --template templates/tufte-handout.tex '
 PANDOC_PAGE = 'pandoc -f markdown+pipe_tables+backtick_code_blocks+fenced_divs+raw_html --toc --toc-depth=1 -s --template templates/template-index.html '
 PANDOC_VARS = f'-V date="$({DATE_CMD})" -V versao="2025/02"'
-MARP_CMD = 'npx @marp-team/marp-cli  --theme templates/slides.css  --allow-local-files --html '
+MARP_CMD = 'npx @marp-team/marp-cli --browser-path vivaldi  --theme templates/slides.css  --allow-local-files --html '
 
 
 src = pathlib.Path('src')
